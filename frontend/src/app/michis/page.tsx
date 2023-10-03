32d3cd0ca53b76eb2fd3002e553b4276e01ef02f
jdavidavendano <jdavidavendano@gmail.com>
2023-10-03 16:58:33 -0500

    some mocked data and some styles

--- frontend/src/app/michis/page.tsx
+++ frontend/src/app/michis/page.tsx
@@ -1,12 +1,42 @@
 // Jona
 import MichiCard from '../../components/michiCard';
 
-export default function Home() {
+import cats from './mockedData';
+
+interface Cat {
+  id: number;
+  name: string;
+  description: string | null;
+  image_url: string | null;
+  age: number | null;
+  personality: string | null;
+  sex: string;
+  health_state: string;
+  is_available: boolean;
+  added_at: string;
+  updated_at: string | null;
+}
+
+export default function Michis() {
   return (
     <main>
       Michis
-
-      <MichiCard />
+      <ul>
+        {cats.cats.map((cat: Cat) => (
+          <li key={cat.id}>
+            <MichiCard
+              name={cat.name}
+              description={cat.description}
+              image_url={cat.image_url}
+              age={cat.age}
+              personality={cat.personality}
+              sex={cat.sex}
+              health_state={cat.health_state}
+              is_available={cat.is_available}
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
