3c53c3c6424024899bf833440ba30ed30c9ee04e
jdavidavendano <jdavidavendano@gmail.com>
2023-10-06 14:23:23 -0500

    Added michi inventory and filters

--- frontend/src/app/michis/page.tsx
+++ frontend/src/app/michis/page.tsx
@@ -1,4 +1,5 @@
-// Jona
+import { Box } from '@mui/material';
+import FilterList from '../../components/filters';
 import MichiCard from '../../components/michiCard';
 
 import cats from './mockedData';
@@ -17,26 +18,38 @@ interface Cat {
   updated_at: string | null;
 }
 
+const cardActionsStyles = {
+  display: 'flex',
+};
+
 export default function Michis() {
   return (
     <main>
-      Michis
-      <ul>
-        {cats.cats.map((cat: Cat) => (
-          <li key={cat.id}>
-            <MichiCard
-              name={cat.name}
-              description={cat.description}
-              image_url={cat.image_url}
-              age={cat.age}
-              personality={cat.personality}
-              sex={cat.sex}
-              health_state={cat.health_state}
-              is_available={cat.is_available}
-            />
-          </li>
-        ))}
-      </ul>
+      <Box sx={cardActionsStyles}>
+        <FilterList />
+        <ul style={{
+          listStyle: 'none',
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '1rem',
+        }}
+        >
+          {cats.cats.map((cat: Cat) => (
+            <li key={cat.id}>
+              <MichiCard
+                name={cat.name}
+                description={cat.description}
+                image_url={cat.image_url}
+                age={cat.age}
+                personality={cat.personality}
+                sex={cat.sex}
+                health_state={cat.health_state}
+                is_available={cat.is_available}
+              />
+            </li>
+          ))}
+        </ul>
+      </Box>
     </main>
   );
 }
--- frontend/src/components/michiCard.tsx
+++ frontend/src/components/michiCard.tsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -17,6 +18,32 @@ const cardActionsStyles = {
   '& button *': {
     margin: '0 0.2rem',
   },
+};
+
+const cardStyles = {
+  width: '300px',
+  height: '340px',
+  '& .nameAndAge': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& > div': {
+      display: 'flex',
+      gap: '0.5rem',
+      margin: 0,
+    },
+  },
+  '& .threedots': {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  '& .health': {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: '0.5rem',
+  },
+
 };
 
 interface MichiCardProps {
@@ -41,7 +68,7 @@ export default function MichiCard({
   is_available,
 }: MichiCardProps) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={cardStyles}>
       <CardMedia
         component="img"
         alt={name}
@@ -49,21 +76,31 @@ export default function MichiCard({
         image={image_url ?? ''}
       />
       <CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="baseline">
+        <Box className="nameAndAge">
           <Typography gutterBottom variant="h5" component="div">
             {name}
           </Typography>
-          <Chip variant="outlined" size="small" label={`${age} años`} />
+          <Box>
+            {
+              sex === 'female'
+                ? <FemaleIcon />
+                : <MaleIcon />
+            }
+            <Chip variant="outlined" size="small" label={`${age} años`} />
+          </Box>
         </Box>
-        <Typography variant="body1" color="text.secondary">
+        <Typography variant="body1" color="text.secondary" className="threedots">
           {description}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" className="threedots">
           {personality}
-          {sex}
-          {health_state}
         </Typography>
-
+        <Box className="health">
+          <HealthAndSafetyIcon />
+          <Typography variant="body2" color="text.secondary">
+            {health_state}
+          </Typography>
+        </Box>
       </CardContent>
       {
         // TODO: puedo marcar como favorito a uno no disponible o no lo debo mostrar?
