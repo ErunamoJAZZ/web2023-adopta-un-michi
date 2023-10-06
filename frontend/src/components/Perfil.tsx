e86634201ca09680cb7739a2c6e05e7e71ca3f8b
Jvier <64610615+kalixtez@users.noreply.github.com>
2023-10-06 18:15:38 -0500

    Update Perfil.tsx
    replaced «'» with «"»

--- frontend/src/components/Perfil.tsx
+++ frontend/src/components/Perfil.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import { Avatar, Button, TextField, Typography } from "@mui/material"
 import { FunctionComponent as FC } from "react"
-import styles from './profile.style.module.css'
+import styles from "./profile.style.module.css"
 
 export interface ProfileProps
 {
@@ -17,23 +17,23 @@ export const Perfil: FC<ProfileProps> = (profile_info: ProfileProps) =>
 
     return (
         <>
-        <div style={{position:'absolute', left:'42%', top:'15%'}}>
-            <Avatar sx={{width: 150, height:150, fontSize:40, textAlign:'justify', left:'20%'}}
-            children={`${profile_info.nombre.split(' ')[0][0].toUpperCase()}
-            ${profile_info.nombre.split(' ')[1][0].toUpperCase()}`}/>
+        <div style={{position:"absolute", left:"42%", top:"15%"}}>
+            <Avatar sx={{width: 150, height:150, fontSize:40, textAlign:"justify", left:"20%"}}
+            children={`${profile_info.nombre.split(" ")[0][0].toUpperCase()}
+            ${profile_info.nombre.split(" ")[1][0].toUpperCase()}`}/>
             {isEditing ? (
                 <>
                 <div>
                 {
                     (Object.keys(profile_info) as (keyof typeof profile_info)[]).map(key => { return (
-                        <TextField size='small' fullWidth margin="normal" label={key} defaultValue={profile_info[key]} sx={{display:'block', padding:'5px'}}></TextField>
+                        <TextField size="small" fullWidth margin="normal" label={key} defaultValue={profile_info[key]} sx={{display:"block", padding:"5px"}}></TextField>
                     )
                     })
                 }
                 </div>
                 <Button onClick={() => setIsEditing(false)}> Guardar </Button></>
             ) : (
-                <Button style={{position:'relative', left:'48%'}} onClick={() => setIsEditing(true)}> Editar </Button>
+                <Button style={{position:"relative", left:"48%"}} onClick={() => setIsEditing(true)}> Editar </Button>
             )}
             
         </div>
