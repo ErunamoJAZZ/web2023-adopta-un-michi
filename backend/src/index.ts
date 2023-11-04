4fe465b91f4164f4fcf52b07c234ad5c49c7c1bd
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-11-04 14:51:45 -0500

    adding postgraphile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response , Application } from 'express';
+import { postgraphile } from 'postgraphile';
 import dotenv from 'dotenv';
 
 //For env File
@@ -8,8 +9,23 @@ const app: Application = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+  res.send('This is a Postgraphile API. Use the /graphql endpoint to access.');
 });
+
+
+app.use(
+  postgraphile(
+    process.env.DATABASE_URL,
+    'michis',
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+  )
+);
+
+
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
