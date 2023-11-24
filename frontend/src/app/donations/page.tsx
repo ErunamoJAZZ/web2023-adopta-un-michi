77b8e51139a566b76c3a0baa585750157727c0e0
jdavidavendano <jdavidavendano@gmail.com>
2023-11-24 16:15:55 -0500

    donations

--- frontend/src/app/donations/page.tsx
+++ frontend/src/app/donations/page.tsx
@@ -1,7 +1,59 @@
+/*
+  donation type
+    money
+      ammount
+    food
+    medicines
+
+*/
+
+import {
+  FormControl, Box, FormLabel, Button, Typography, ButtonGroup, MenuItem, Select,
+} from '@mui/material';
+
+const donationStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  '& .donations': {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+};
+
 export default function Home() {
   return (
     <main>
-      Donaciones
+      <Box sx={donationStyles}>
+        <Typography id="donate-form" variant="h6" component="h2">
+          ¿Quieres donarle a los michis?
+        </Typography>
+        <Typography id="donate-form" component="p">
+          Llenando este formulario,
+          nos pondremos en contacto contigo para que puedas realizar la donación.
+        </Typography>
+        <FormControl className="form">
+          <Box className="donations">
+            <FormLabel>
+              ¿Tus ventanas están cerradas o protegidas para que el gato no escape?
+            </FormLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Dinero</MenuItem>
+              <MenuItem value={20}>Comida</MenuItem>
+              <MenuItem value={30}>Medicinas</MenuItem>
+            </Select>
+          </Box>
+        </FormControl>
+      </Box>
     </main>
   );
 }
