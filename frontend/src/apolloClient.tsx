25a847fab4d690e54c123d672096c2e2717a3a5e
C. Daniel Sanchez R <cdsanchezr@unal.edu.co>
2023-11-27 00:17:18 -0500

    fix errors

--- frontend/src/apolloClient.tsx
+++ frontend/src/apolloClient.tsx
@@ -1,22 +1,32 @@
 'use client';
 
-// ^ this file needs the 'use client' pragma
-import { ApolloLink, HttpLink } from '@apollo/client';
+import {
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client';
 import {
   ApolloNextAppProvider,
   NextSSRInMemoryCache,
   NextSSRApolloClient,
   SSRMultipartLink,
 } from '@apollo/experimental-nextjs-app-support/ssr';
+import { onError } from '@apollo/client/link/error';
+import { useRouter } from 'next/navigation';
+
+
 
 // have a function to create a client for you
 function makeClient() {
-  const token = localStorage.getItem('tokenMichis');
-
+  const router = useRouter();
+  let token: string | null = '';
+  if (typeof window !== 'undefined') {
+    token = localStorage.getItem('tokenMichis');
+  }
   const headers = token
     ? {
       Authorization: `Bearer ${token}`,
     } : undefined;
+
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: `${process.env.SERVER_URL}/graphql`,
@@ -25,18 +35,39 @@ function makeClient() {
     fetchOptions: { cache: 'no-store' },
   });
 
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (networkError) {
+      if (networkError.statusCode === 403) {
+        console.log('REMOVIENDO TOKEN', token);
+        localStorage.removeItem('tokenMichis');
+        router.push('/login');
+      }
+    }
+    if (graphQLErrors) {
+      const denied= graphQLErrors.some((err) => err.message.includes('permission denied'))
+      if (denied) {
+        localStorage.removeItem('tokenMichis');
+        router.push('/login');
+      }
+    }
+  });
+
   return new NextSSRApolloClient({
     // use the `NextSSRInMemoryCache`, not the normal `InMemoryCache`
     cache: new NextSSRInMemoryCache(),
     link:
       typeof window === 'undefined'
         ? ApolloLink.from([
+          errorLink,
           new SSRMultipartLink({
             stripDefer: true,
           }),
           httpLink,
         ])
-        : httpLink,
+        : ApolloLink.from([
+          errorLink,
+          httpLink,
+        ]),
   });
 }
 
